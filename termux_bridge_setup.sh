e8f496aeb5ca4e024d6a2001dfb57af652236abc
Divided by Zer0 <mail@dbzer0.com>
2022-09-26 10:27:22 +0200

    Added termux_bridge_setup.sh

---
+++ termux_bridge_setup.sh
@@ -0,0 +1,8 @@
+pkg update
+termux-setup-storage
+pkg install python openssh git
+git clone https://github.com/db0/KoboldAI-Horde.git
+cd KoboldAI-Horde
+pip install -r bridge_requirements.txt --user
+cp clientData_template.py clientData.py 
+echo "kai_url = $1" >> clientData.py
