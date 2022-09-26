221584b435decc1aedc0aa2b4d535d349e1924f4
Divided by Zer0 <mail@dbzer0.com>
2022-09-26 10:50:51 +0200

    dirs

--- termux_bridge_setup.sh
+++ termux_bridge_setup.sh
@@ -3,12 +3,12 @@ termux-setup-storage
 yes | pkg install python openssh git
 git clone https://github.com/db0/KoboldAI-Horde.git ${HOME}/KoboldAI-Horde
 cd ${HOME}/KoboldAI-Horde
-pip install -r bridge_requirements.txt --user
-cp clientData_template.py clientData.py 
+pip install -r ${HOME}/KoboldAI-Horde/bridge_requirements.txt --user
+cp ${HOME}/KoboldAI-Horde/clientData_template.py ${HOME}/KoboldAI-Horde/clientData.py 
 
 if [[ "$1" ]]; then
-    echo "" >> clientData.py
-    echo "kai_url = $1" >> clientData.py
+    echo "" >> ${HOME}/KoboldAI-Horde/clientData.py
+    echo "kai_url = $1" >> ${HOME}/KoboldAI-Horde/clientData.py
 else
     echo 'No Colab URL provided. Please manually edit your clientData.py and add your colab url in the 'kai_url' variable.'
 fi
